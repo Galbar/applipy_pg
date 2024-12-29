c31352a7cdc3bbb79805b7fc8df3a215d3eebd4f
Alessio Linares <github@alessio.cc>
2024-12-29 12:13:00 -0500

    Fix mypy issues

--- applipy_pg/migrations/module.py
+++ applipy_pg/migrations/module.py
@@ -11,17 +11,24 @@ from .handle import MigrationsHandle
 from .repository import Repository
 from .migration import PgMigration, find_migrations
 from applipy_pg import PgModule
+from typing import cast
 
 
 class PgMigrationsModule(Module):
+    _config: Config
+
     def __init__(self, config: Config) -> None:
-        self._config = config['pg.migrations']
+        self._config = cast(Config, config['pg.migrations'])
 
     def configure(self, bind: BindFunction, register: RegisterFunction) -> None:
         connection_name = self._config.get("connection")
         if connection_name is not None and type(connection_name) is not str:
             raise TypeError("Config value `pg.migrations.connection` must be a string or None")
-        bind(with_names(Repository, {"pool": connection_name}))
+        if connection_name:
+            bind(with_names(Repository, {"pool": connection_name}))
+        else:
+            bind(Repository)
+
         register(MigrationsHandle)
         self._bind_migrations_from_config(bind)
 
